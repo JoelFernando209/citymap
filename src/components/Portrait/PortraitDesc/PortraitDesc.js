18cd4058fb40b712033b2a790634c90ed166183c
Joel Fernando Dominguez Tapia <joeldomingueztapia@gmail.com>
2021-02-12 21:21:08 -0800

    Add Typewriter effect

--- src/components/Portrait/PortraitDesc/PortraitDesc.js
+++ src/components/Portrait/PortraitDesc/PortraitDesc.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TypeWriter from 'react-typewriter';
 
 import classes from './PortraitDesc.module.scss';
 
 import SearchBox from './SearchBox/SearchBox';
 
-const PortraitDesc = () => (
-  <div className={classes.PortraitDesc}>
-    <div className={classes.PortraitTitle}>Search Your Destination</div>
-    
-    <div className={classes.PortraitText}>
-      Then select departure date, how long the trip will be and how many destinations to include. Users can also select if they want to look at cities in Europe, the European Union or just the Schengen Zone.
+const PortraitDesc = () => {
+  return (
+    <div className={classes.PortraitDesc}>
+      <div className={classes.PortraitTitle}>
+        <TypeWriter
+          fixed
+          typing={1}
+        >
+          Search Your Destination
+        </TypeWriter>
+      </div>
+      
+      <div className={classes.PortraitText}>
+        Then select departure date, how long the trip will be and how many destinations to include. Users can also select if they want to look at cities in Europe, the European Union or just the Schengen Zone.
+      </div>
+      
+      <SearchBox />
     </div>
-    
-    <SearchBox />
-  </div>
-);
+  )
+};
 
 export default PortraitDesc;
